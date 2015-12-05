10e8ce3613ec31c792b83fa1c4991b9d1d0f5997
[Jennifer Ford] <[jennifer.ford.design@gmail.com]>
2015-12-05 07:35:49 -0500

    Added EditorConfig

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$ (document) .ready (function () {
+
+<script type="text/javascript">
+              //initiating jQuery
+              jQuery(function($) {
+                $(document).ready( function() {
+                  $('.header').stickUp();
+                });
+              });
+
+            </script>
+
+        });
